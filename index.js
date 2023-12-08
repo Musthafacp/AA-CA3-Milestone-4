8b42ce9db3c99df6ad38b7f826caf4e9387d6359
musthafa <musthafa.cp@kalvium.community>
2023-12-08 16:24:08 +0530

    updated.

--- index.js
+++ index.js
@@ -1,52 +1,79 @@
-let API = "https://www.themealdb.com/api/json/v1/1/random.php";
-
 let op = '';
+var mainhomepage = document.querySelector(".mainhomepage");
+var ingrediants = document.getElementById("ingrediants");
+var body = document.querySelector(".body");
+var ingrediants_button = document.querySelector(".ingrediants_button");
 
-// let cell_image_name = document.querySelector(".cell_image_name")
+var dish = document.getElementById("dish");
+var dishImage = document.getElementById("dishImage");
 
-for(let i =0 ;i < 8 ; i++){
-    
+for (let i = 0; i < 8; i++) {
     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-
-    .then(response => response.json())
-
-    .then(data => {
-        getEightRandom(data)
-    })
-    .catch(error => {
-    console.log('ERROR', error);
-    });
-
+        .then(response => response.json())
+        .then(data => {
+            getEightRandom(data);
+        })
+        .catch(error => {
+            console.log('ERROR', error);
+        });
 }
 
-var mainhomepage = document.querySelector(".mainhomepage")
-var ingrediants = document.getElementById("ingrediants")
-var body = document.querySelector(".body")
-var ingrediants_button = document.querySelector(".ingrediants_button")
 
-var dish = document.getElementById("dish")
 
-function getEightRandom(data){
-        let main = document.querySelector('.random_meals');
-        op += `<div class="cell">
-        <div class="cell_image"><img src="${data.meals[0].strMealThumb}" width="230px" alt=""></div>
+function getEightRandom(data) {
+    let main = document.querySelector('.random_meals');
+    op += `<div class="cell">
+        <div class="cell_image"><img src="${data.meals[0].strMealThumb}" width="100%" height="100%" alt=""></div>
         <h2 class="cell_image_name">${data.meals[0].strMeal}</h2>
-        </div>` 
-        main.innerHTML = op;
-        var cells = document.querySelectorAll(".cell")
-        cells.forEach(element => {
-            element.addEventListener('click',()=>{
+    </div>`;
 
-                    console.log(data)
-                    ingrediants.style.visibility = "visible"
-                    mainhomepage.style.filter = 'blur(8px)';
-                    body.style.overflow = 'hidden'
-            })
+    main.innerHTML = op;
+
+    var cells = document.querySelectorAll(".cell");
+    cells.forEach(element => {
+        element.addEventListener('click', () => {
+
+            let clickedMealName = element.querySelector('.cell_image_name').textContent;
+            let clickedMealImage = element.querySelector('.cell_image img').src;
+
+            dish.innerText = clickedMealName;
+            dishImage.src = clickedMealImage;
+
+            console.log(clickedMealName)
+            fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${clickedMealName}`)
+                .then(response => response.json())
+                .then(data => {
+                    displayIngredients(data);
+                })
+                .catch(error => {
+                    console.log('ERROR', error);
+                });
+
+            ingrediants.style.visibility = "visible";
+            mainhomepage.style.filter = 'blur(8px)';
+            // body.style.overflow = 'hidden';
         });
+    });
 
-        ingrediants_button.addEventListener("click",()=>{
-            ingrediants.style.visibility = "hidden"
-            mainhomepage.style.filter = 'blur(0px)';
-            body.style.overflow = 'auto'
-        })
-    }
+    ingrediants_button.addEventListener("click", () => {
+        ingrediants.style.visibility = "hidden";
+        mainhomepage.style.filter = 'blur(0px)';
+        body.style.overflow = 'auto';
+    });
+}
+
+var inglist = document.getElementById("inglist")
+
+function displayIngredients(data) {
+    const array = data.meals[0];
+    console.log(array)
+    let temp = '';
+    Object.keys(array).forEach(key => {
+        if (key.startsWith("strIngredient")) {
+            console.log(array[key]);
+            temp += `<li class="li">${array[key]}</li>`
+        }
+    });
+    inglist.innerHTML = temp;
+}
+
--- main.js
+++ main.js
@@ -6,6 +6,10 @@ let main = document.querySelector('.Latest');
 let Popular = document.querySelector(".Popular")
 let popHead = document.getElementById("popHead")
 
+let searchBox1 = document.getElementById("inputBox")
+var inglist = document.getElementById("inglist")
+
+var dishname = document.querySelector(".dishname")
 
 firsthead.style.display = "none";
 
@@ -19,7 +23,6 @@ if (flag == 0) {
             .then(response => response.json())
 
             .then(data => {
-                // console.log(data);
                 previoussearch(data)
             })
             .catch(error => {
@@ -29,102 +32,211 @@ if (flag == 0) {
     }
 
     let opp = '';
+    let mainhomepage = document.querySelector(".backgroundofING");
+    let ingrediants = document.getElementById("ingrediants");
+    let body = document.querySelector(".body");
+    var ingrediants_button = document.querySelector(".ingrediants_button")
 
 
-
-    
-let mainhomepage = document.querySelector(".backgroundofING")
-let ingrediants = document.getElementById("#ingrediants")
-let body = document.querySelector(".body")
-
-
-/* 
-.backgroundofING{
-    filter: blur(8px);
-  -webkit-filter: blur(8px); 
-}
-
-body{
-    overflow: hidden;
-}
-
-
-
-
-#ingrediants{
-    position: relative;
-    bottom: 1500px;
-} */
+    var dish = document.querySelector(".dishname");
+    var dishImage = document.getElementById("dishIMG");
 
 
     function previoussearch(data) {
         opp += `<div class="Items cell">
         <img src="${data.meals[0].strMealThumb}" id="cell_images" alt="">
         <h1 id="cell_images_name">${data.meals[0].strMeal}</h1>
-    </div>`
+        </div>`;
         main.innerHTML = opp;
-    }
-    flag = 1;
+        let cel = document.querySelectorAll(".Items");
+        cel.forEach(element => {
+            element.addEventListener('click', () => {
 
-    // let cel = document.querySelectorAll(".Items")
-    //     cel.forEach(element => {
-    //         element.addEventListener('click',()=>{
-    //             // ingrediants.style.position = 'relative'
-    //             // ingrediants.style.bottom = '700px'
-    //             // ingrediants.style.display = "grid"
-    //             mainhomepage.style.filter = 'blur(8px)';
-    //             body.style.overflow = 'hidden'
-    //         })
-    //     });
-}
+                let clickedMealName = element.querySelector('#cell_images_name').textContent;
+                let clickedMealImage = element.querySelector('#cell_images').src;
 
+                dish.innerText = clickedMealName;
+                dishImage.src = clickedMealImage;
 
+                fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${clickedMealName}`)
+                .then(response => response.json())
+                .then(data => {
+                    displayIngredients(data);
+                })
+                .catch(error => {
+                    console.log('ERROR', error);
+                });
 
+                ingrediants.style.visibility = "visible"
+                ingrediants.style.position = 'relative';
+                ingrediants.style.bottom = '80em';
+                ingrediants.style.display = "grid";
+                mainhomepage.style.filter = 'blur(8px)';
+                // body.style.overflow = 'hidden';
+                ingrediants.style.marginLeft = "15%";
+                ingrediants.style.width = "70%"
+            })
 
-
-search.addEventListener("click", () => {
-    let searchBox = document.getElementById("inputBox")
-    var searchedValue = searchBox.value;
-
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchedValue}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            searchedItems(data)
-            main.style.display = 'none';
-            fhead.style.display = "none"
-            Popular.style.display = "none"
-            popHead.style.display = "none";
-        })
-        .catch(error => {
-            console.log('ERROR', error);
-            alert("CATOGORIES NOT FOUND")
+            ingrediants_button.addEventListener("click", () => {
+                ingrediants.style.visibility = "hidden"
+                mainhomepage.style.filter = 'blur(0px)';
+                body.style.overflow = 'auto'
+            })
         });
 
-})
+        ingrediants_button.addEventListener("click", () => {
+            ingrediants.style.visibility = "hidden"
+            mainhomepage.style.filter = 'blur(0px)';
+            body.style.overflow = 'auto'
+        })
+    }
 
-function searchedItems(data) {
-    let op = '';
-    const searched = document.querySelector(".Searched");
-    firsthead.style.display = "block";
-    data.meals.forEach(e => {
-        op +=
-            `<div class="SearchedItems cell">
+
+    searchBox1.addEventListener('keyup' ,()=>{
+        let searchBox = document.getElementById("inputBox")
+        var searchedValue = searchBox.value;
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchedValue}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                searchedItems(data)
+                main.style.display = 'none';
+                fhead.style.display = "none"
+                Popular.style.display = "none"
+                popHead.style.display = "none";
+            })
+            .catch(error => {
+                console.log('ERROR', error);
+            });
+    })
+
+    
+    searchBox1.addEventListener('keyup' ,()=>{
+        let searchBox = document.getElementById("inputBox")
+        var searchedValue = searchBox.value;
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchedValue}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                searchedItems(data)
+                main.style.display = 'none';
+                fhead.style.display = "none"
+                Popular.style.display = "none"
+                popHead.style.display = "none";
+            })
+            .catch(error => {
+                console.log('ERROR', error);
+            });
+    })
+
+
+
+    search.addEventListener("click", () => {
+        let searchBox = document.getElementById("inputBox")
+        var searchedValue = searchBox.value;
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${searchedValue}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                searchedItems(data)
+                main.style.display = 'none';
+                fhead.style.display = "none"
+                Popular.style.display = "none"
+                popHead.style.display = "none";
+            })
+            .catch(error => {
+                console.log('ERROR', error);
+            });
+
+    })
+
+
+    function searchedItems(data) {
+        let op = '';
+        const searched = document.querySelector(".Searched");
+        firsthead.style.display = "block";
+        data.meals.forEach(e => {
+            op +=
+                `<div class="SearchedItems cell">
                 <img src="${e.strMealThumb}" id="searched_item_images" alt="">
                 <h1 id="searched_item_images_name">${e.strMeal}</h1>
             </div>`;
-    });
-    searched.innerHTML = op;
+
+        });
+        searched.innerHTML = op;
+
+        let cel = document.querySelectorAll(".SearchedItems");
+        cel.forEach(element => {
+            element.addEventListener('click', () => {
+
+                let clickedMealname = element.querySelector('#searched_item_images_name').textContent;
+                let clickedMealimage = element.querySelector('#searched_item_images').src;
+
+                dish.innerText = clickedMealname;
+                dishImage.src = clickedMealimage;
+                
+                fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${clickedMealname}`)
+                .then(response => response.json())
+                .then(data => {
+                    displayIngredients(data);
+                })
+                .catch(error => {
+                    console.log('ERROR', error);
+                });
 
 
-    let cells = document.querySelectorAll(".SearchedItems")
-        cells.forEach(element => {
-            element.addEventListener('click',()=>{
-                // ingrediants.style.position = 'relative'
-                // ingrediants.style.bottom = '700px'
-                // ingrediants.style.display = "grid"
+                ingrediants.style.visibility = "visible"
+                ingrediants.style.position = 'relative';
+                ingrediants.style.bottom = '-60em';
+                ingrediants.style.right = '8em';
+                ingrediants.style.display = "grid";
                 mainhomepage.style.filter = 'blur(8px)';
-                body.style.overflow = 'hidden'
+                body.style.height = '100px';
+            })
+
+            ingrediants_button.addEventListener("click", () => {
+                ingrediants.style.visibility = "hidden"
+                mainhomepage.style.filter = 'blur(0px)';
+                body.style.overflow = 'auto'
             })
         });
+
+        ingrediants_button.addEventListener("click", () => {
+            ingrediants.style.visibility = "hidden"
+            mainhomepage.style.filter = 'blur(0px)';
+            body.style.overflow = 'auto'
+        })
+    }
+
+    // var inglist = document.getElementById("inglist")
+
+    function displayIngredients(data) {
+        const array = data.meals[0];
+        console.log(array)
+        let temp = '';
+        Object.keys(array).forEach(key => {
+            if (key.startsWith("strIngredient")) {
+                console.log(array[key]);
+                temp +=   `<li class="li">${array[key]}</li>`
+            }
+        });
+        inglist.innerHTML = temp;
+    }
+}
+
+
+function displayIngredients(data) {
+    const array = data.meals[0];
+    console.log(array)
+    let temp = '';
+    Object.keys(array).forEach(key => {
+        if (key.startsWith("strIngredient")) {
+            console.log(array[key]);
+            temp +=   `<li class="li">${array[key]}</li>`
+        }
+    });
+    inglist.innerHTML = temp;
 }
